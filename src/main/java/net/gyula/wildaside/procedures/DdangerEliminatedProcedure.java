55a380b049395ad06b8e2c9c376d77ab6b194384
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-11-20 22:17:37 +0100

    new advancements

---
+++ src/main/java/net/gyula/wildaside/procedures/DdangerEliminatedProcedure.java
@@ -0,0 +1,48 @@
+package net.gyula.wildaside.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.world.BlockEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import net.gyula.wildaside.init.WildasideModBlocks;
+
+import javax.annotation.Nullable;
+
+import java.util.Iterator;
+
+@Mod.EventBusSubscriber
+public class DdangerEliminatedProcedure {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent event) {
+		execute(event, event.getWorld(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((world.getBlockState(new BlockPos(x, y, z))).getBlock() == WildasideModBlocks.NATURAL_SPORE_BLASTER.get()) {
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("wildaside:danger_eliminated"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					Iterator _iterator = _ap.getRemainingCriteria().iterator();
+					while (_iterator.hasNext())
+						_player.getAdvancements().award(_adv, (String) _iterator.next());
+				}
+			}
+		}
+	}
+}
