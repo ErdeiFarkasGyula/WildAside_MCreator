f3a3527bdad43835899358bf2461eb511b8555d8
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-11-25 14:51:07 +0100

    backup

--- src/main/java/net/gyula/wildaside/client/model/Modelentorium_gauntlet.java
+++ src/main/java/net/gyula/wildaside/client/model/Modelentorium_gauntlet.java
@@ -26,29 +26,34 @@ public class Modelentorium_gauntlet<T extends Entity> extends EntityModel<T> {
 			"main");
 	public final ModelPart gaunletRight;
 	public final ModelPart gaunletLeft;
+	public final ModelPart gauntletMiddle;
 
 	public Modelentorium_gauntlet(ModelPart root) {
 		this.gaunletRight = root.getChild("gaunletRight");
 		this.gaunletLeft = root.getChild("gaunletLeft");
+		this.gauntletMiddle = root.getChild("gauntletMiddle");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
-		PartDefinition gaunletRight = partdefinition.addOrReplaceChild(
-				"gaunletRight", CubeListBuilder.create().texOffs(0, 0).addBox(-8.5F, -17.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
-						.texOffs(0, 0).addBox(-8.75F, -12.0F, -1.5F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition gaunletRight = partdefinition
+				.addOrReplaceChild(
+						"gaunletRight", CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
+								.texOffs(0, 0).addBox(-2.75F, 5.0F, -1.5F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
+						PartPose.offset(-9.0F, 10.0F, 0.0F));
 		PartDefinition finger1_r1 = gaunletRight.addOrReplaceChild("finger1_r1",
 				CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, 6.6F, 2.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(8.0F, 0.0F, -8.0F, 0.0F, 0.0F, -0.3927F));
+				PartPose.offsetAndRotation(14.0F, 17.0F, -8.0F, 0.0F, 0.0F, -0.3927F));
 		PartDefinition gaunletLeft = partdefinition.addOrReplaceChild("gaunletLeft",
-				CubeListBuilder.create().texOffs(0, 0).addBox(-8.5F, -17.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
-						.addBox(-8.75F, -12.0F, -1.5F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+				CubeListBuilder.create().texOffs(0, 0).addBox(-2.5726F, 0.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-2.8226F, 5.0F, -1.5F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(9.0F, 10.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 		PartDefinition finger2_r1 = gaunletLeft.addOrReplaceChild("finger2_r1",
 				CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, 6.35F, 2.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(8.0F, 0.0F, -8.0F, 0.0F, 0.0F, -0.3927F));
+				PartPose.offsetAndRotation(13.9274F, 17.0F, -8.0F, 0.0F, 0.0F, -0.3927F));
+		PartDefinition gauntletMiddle = partdefinition.addOrReplaceChild("gauntletMiddle", CubeListBuilder.create(),
+				PartPose.offset(0.0F, 9.0F, 0.0F));
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
@@ -57,6 +62,7 @@ public class Modelentorium_gauntlet<T extends Entity> extends EntityModel<T> {
 			float blue, float alpha) {
 		gaunletRight.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		gaunletLeft.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		gauntletMiddle.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
