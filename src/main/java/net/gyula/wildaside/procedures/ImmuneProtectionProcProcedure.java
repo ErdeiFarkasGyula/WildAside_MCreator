67b7da059860a11eb3fe5b4a6864e30e3136873b
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-12-12 21:24:01 +0100

    remade some textures ^^

--- src/main/java/net/gyula/wildaside/procedures/ImmuneProtectionProcProcedure.java
+++ src/main/java/net/gyula/wildaside/procedures/ImmuneProtectionProcProcedure.java
@@ -2,6 +2,7 @@ package net.gyula.wildaside.procedures;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
 
 import net.gyula.wildaside.init.WildasideModMobEffects;
 
@@ -12,6 +13,10 @@ public class ImmuneProtectionProcProcedure {
 		if (entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(WildasideModMobEffects.CONTAMINATION.get()) : false) {
 			if (entity instanceof LivingEntity _entity)
 				_entity.removeEffect(WildasideModMobEffects.CONTAMINATION.get());
+		}
+		if (entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.POISON) : false) {
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(MobEffects.POISON);
 		}
 	}
 }
