c52f29ea7c9d50550fbc224106981f151e8ad110
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-12-29 23:30:02 +0100

    done for today (there are errors to be fixed)

--- src/main/java/net/gyula/wildaside/block/RedGlowingHickoryLeavesBlock.java
+++ src/main/java/net/gyula/wildaside/block/RedGlowingHickoryLeavesBlock.java
@@ -1,11 +1,17 @@
 
 package net.gyula.wildaside.block;
 
+import java.util.function.ToIntFunction;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.Level;
@@ -14,14 +20,21 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.core.BlockPos;
 
 import net.gyula.wildaside.procedures.RedGlowingHickoryLeavesBlockDestroyedByPlayerProcedure;
+import net.minecraft.world.level.block.RedstoneTorchBlock;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class RedGlowingHickoryLeavesBlock extends LeavesBlock {
+
+	public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
+
 	public RedGlowingHickoryLeavesBlock() {
-		super(BlockBehaviour.Properties.of(Material.LEAVES, MaterialColor.COLOR_RED).sound(SoundType.GRASS).strength(0.3f).noOcclusion());
+		super(BlockBehaviour.Properties.of(Material.LEAVES, MaterialColor.COLOR_RED).sound(SoundType.GRASS).lightLevel(glowingLeavesLightlevel(7)).strength(0.3f).noOcclusion());
+		this.registerDefaultState(this.defaultBlockState().setValue(LIT, Boolean.valueOf(false)));
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+this.registerDefaultState(this.defaultBlockState().setValue(LIT, Boolean.valueOf(false)));
 		return 1;
 	}
 
@@ -31,4 +44,17 @@ public class RedGlowingHickoryLeavesBlock extends LeavesBlock {
 		RedGlowingHickoryLeavesBlockDestroyedByPlayerProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity);
 		return retval;
 	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+		pBuilder.add(LIT);
+	}
+
+	private static ToIntFunction<BlockState> glowingLeavesLightlevel (int pLightValue) {
+      return (gLLl) -> {
+         return gLLl.getValue(BlockStateProperties.LIT) ? pLightValue : 0;
+      };
+   }
+
+	
 }
