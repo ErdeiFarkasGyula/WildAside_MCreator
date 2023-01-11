e343658f19fd0b29271f2bc79bbced8f853b91dc
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2023-01-11 17:22:04 +0100

    fixes!

--- src/main/java/net/gyula/wildaside/procedures/HickoryPlanksPlacedProcedure.java
+++ src/main/java/net/gyula/wildaside/procedures/HickoryPlanksPlacedProcedure.java
@@ -7,20 +7,26 @@ import net.minecraft.core.BlockPos;
 
 public class HickoryPlanksPlacedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (0.37 > Math.random()) {
-			{
-				BlockPos _pos = new BlockPos(x, y, z);
-				BlockState _bs = world.getBlockState(_pos);
-				if (_bs.getBlock().getStateDefinition().getProperty("is_yellow") instanceof BooleanProperty _booleanProp)
-					world.setBlock(_pos, _bs.setValue(_booleanProp, (true)), 3);
+		boolean is_set = false;
+		if (is_set == false) {
+			if (0.37 > Math.random()) {
+				{
+					BlockPos _pos = new BlockPos(x, y, z);
+					BlockState _bs = world.getBlockState(_pos);
+					if (_bs.getBlock().getStateDefinition().getProperty("is_yellow") instanceof BooleanProperty _booleanProp)
+						world.setBlock(_pos, _bs.setValue(_booleanProp, (true)), 3);
+				}
+			} else {
+				{
+					BlockPos _pos = new BlockPos(x, y, z);
+					BlockState _bs = world.getBlockState(_pos);
+					if (_bs.getBlock().getStateDefinition().getProperty("is_yellow") instanceof BooleanProperty _booleanProp)
+						world.setBlock(_pos, _bs.setValue(_booleanProp, (false)), 3);
+				}
 			}
-		} else {
-			{
-				BlockPos _pos = new BlockPos(x, y, z);
-				BlockState _bs = world.getBlockState(_pos);
-				if (_bs.getBlock().getStateDefinition().getProperty("is_yellow") instanceof BooleanProperty _booleanProp)
-					world.setBlock(_pos, _bs.setValue(_booleanProp, (false)), 3);
-			}
+		}
+		if (is_set == false) {
+			is_set = true;
 		}
 	}
 }
